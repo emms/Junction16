e2032aa2bcc1098a412fe3fb546c56cb430746e6
Angelos <angelos.arnis@musicinfo.fi>
2016-08-13 15:04:23 +0300

    challenges

--- tracks/future-mobility/index.php
+++ tracks/future-mobility/index.php
@@ -13,16 +13,20 @@
       </header>
 
     <div class="text">
-        <h2 class="text-center hostedby"><span style="font-weight:700;">HOSTED</span> BY</h2>
+        <h2 class="text-center hostedby">HOSTED BY</h2>
         <div >
             <div class="col-xs-4 col-sm-4"><div class="partner-logo" style=""></div></div>
             <div class="col-xs-4 col-sm-4"><div class="partner-logo embers" style=""></div></div>
             <div class="col-xs-4 col-sm-4"><div class="partner-logo" style=""></div></div>
         </div>
         <div class="clear"></div>
-
-        <!--<h2 class="text-center" style="margin-top:100px;"><span style="font-weight:700;">CHALLENGE</span></h2>-->
       <p>From self driving cars to smart cities software and sensor technology is changing the way we move around. Hate traffic? Us too. Hate pollution? Same. Hate finding a parking spot? Yep. Join the Future Mobility track and solve these problems. Apply now!</p>
+      <h2 class="text-center" style="margin-top:100px;"><span style="font-weight:700;">CHALLENGE</span></h2>
+      <p>Connect your devices and applications to your city! EMBERS paves the way for innovative solutions that answer the mobility challenges of the future. Using the Mobility Platform and its open APIs, any developer is capable of seamlessly connecting the physical world with new services and applications. Help people planning their journeys and moving around with their smartphone, improve the quality of life or enrich the context around us with more information and events from new devices; the possibilities are infinite!</p>
+      <p>Participants will have access to the EMBERS infrastructure which consists of hundreds of sensor nodes, tools for sensor emulation, real city data and historical data sets. All available data both EMBERS or your own can be feed to the platform, which provides the information through open and well documented APIs.</p>
+      <p>Integration of data is the key to success! Show us how you would use EMBERS infrastructure to connect the physical world with innovative services and applications that improves the mobility experiences of people in smart cities, improving their quality of life.</p>
+      <p>EMBERS is a European project that aims to stimulate the smart city ecosystem in Europa. While providing a IoT middleware with open APIs, we’re seeking for developers that reveal the strong potential of the platform and integrate new data sets as well as to design innovative applications and solutions.</p>
+      <p>Judging criteria for winning the prize are the creativity of using the EMBERS APIs to solve technical complex challenges that  addressing the mobility and transport sector.</p>
       <div class="text-center"><div class="button" style="margin: 0 auto;"><a href="https://register.hackjunction.com" style="color: rgb(91, 200, 215);">APPLY</a></div></div>
       <?php
           include '../tpl/pagination.php';
--- tracks/intelligent-buildings/index.php
+++ tracks/intelligent-buildings/index.php
@@ -13,7 +13,7 @@
       </header>
 
     <div class="text">
-        <h2 class="text-center hostedby"><span style="font-weight:700;">HOSTED</span> BY</h2>
+        <h2 class="text-center hostedby">HOSTED BY</h2>
         <div >
             <div class="col-xs-4 col-sm-4"><div class="partner-logo" style=""></div></div>
             <div class="col-xs-4 col-sm-4"><div class="partner-logo tieto" style=""></div></div>
@@ -21,8 +21,20 @@
         </div>
         <div class="clear"></div>
 
-        <!--<h2 class="text-center" style="margin-top:100px;"><span style="font-weight:700;">CHALLENGE</span></h2>-->
       <p>Our buildings are getting smarter. The last few years have been focused on smart cars but buildings are not far behind. With various sensors such as temperature, ultrasound, video, and motion many processes can be automated and consumables reduced. Want to help take buildings into the digital age? Join now!</p>
+      <h2 class="text-center" style="margin-top:100px;"><span style="font-weight:700;">CHALLENGE</span></h2>
+      <p>Maintaining and keeping offices running smoothly is changing radically. With the help of sensor data, the same person may in the future deal with security issues, replace broken light sources, add some paper into the rest room, clean a soiled floor in lobby and put some more beans to the coffee machine – without a single supervisor assigning and overseeing each task.</p>
+      <p>Using data from 6000 sensors at Tieto HQ building in Keilalahti, create a kick-ass mobile app that supports the maintenance staff in their daily workflow – one that will send their supervisors to early retirement.</p>
+      <p>The sensors relevant for maintenance work at Tieto HQ include:</p>
+      <p>
+        -Air Quality (temperature, relative humidity, CO2, VOC, PM)<br>
+        -Video (face recognition, people’s direction and routes)<br>
+        -Indoor Positioning (all people, assets, access control, equipment)<br>
+        -Ultrasound (measuring consumables: toilet paper, hand tissue, waste bins, parking spots, office supplies)<br>
+        -Motion (desk usage, meeting room usage, office space usage, light, heating and AC-usage)<br>
+        -Energy consumption (real time energy consumption of every building floor)<br>
+        -Building Automation (technical data from building hardware; AC, heating, ventilation, smoke detection, thermostats, water consumption, etc.)<br>
+      </p>
       <div class="text-center"><div class="button" style="margin: 0 auto;"><a href="https://register.hackjunction.com" style="color: rgb(91, 200, 215);">APPLY</a></div></div>
       <?php
           include '../tpl/pagination.php';
